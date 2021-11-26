9d3405a29da5ef85c2e7f83d2420e5a392e926ad
Omkar Athavale <oa1234@live.com>
2021-11-26 22:57:51 +0000

    added tests and required ICCfactory for 2D large with neural

---
+++ test/TestLarge2D_Neural.hpp
@@ -0,0 +1,125 @@
+#ifndef TESTLARGE2D_NEURAL_HPP_
+#define TESTLARGE2D_NEURAL_HPP_
+
+#define PROBLEM_SPACE_DIM 2
+#define PROBLEM_ELEMENT_DIM 2
+/**
+ * @file
+ * This test runs a minimal simulation with Simplified Imtiaz cells in a 2D mesh
+ */
+
+#include <cxxtest/TestSuite.h>
+/* Most Chaste code uses PETSc to solve linear algebra problems.  This involves starting PETSc at the beginning of a test-suite
+ * and closing it at the end.  (If you never run code in parallel then it is safe to replace PetscSetupAndFinalize.hpp with FakePetscSetup.hpp)
+ */
+
+#include "Debug.hpp"
+
+#include "ChastePoint.hpp"
+#include "../src/ICCFactory_Large2D_Neural.hpp"
+
+#include "../src/BidomainProblemNeural.hpp"
+
+#include "DistributedTetrahedralMesh.hpp"
+#include "TrianglesMeshReader.hpp"
+
+
+#include "PetscSetupAndFinalize.hpp"
+
+class TestMinimal3D : public CxxTest::TestSuite
+{
+  public:
+  void TestMinimalSimulation()
+  {
+
+    // -------------- OPTIONS ----------------- //
+    std::string mesh_ident = "MeshNetwork-2D-2147Nodes-4160Elems";
+    std::string output_dir = mesh_ident + "-2D_Large_Neural";
+    unsigned bath_attr = 0;
+    unsigned icc_attr = 1;
+    double duration = 20000.0;      // ms
+    double print_step = 50.0;        // ms
+    // ---------------------------------------- //
+
+    // Mesh location
+    std::string mesh_dir = "projects/mesh/Rect2D/sp0-75/" + mesh_ident;
+    TrianglesMeshReader<PROBLEM_ELEMENT_DIM,PROBLEM_SPACE_DIM> mesh_reader(mesh_dir.c_str());
+
+    // Initialise mesh variables
+    std::set<unsigned> iccNodes;
+    unsigned nElements = 0;
+    DistributedTetrahedralMesh<PROBLEM_ELEMENT_DIM,PROBLEM_SPACE_DIM> mesh;
+
+    // Cell labels
+    std::set<unsigned> ICC_id;
+    ICC_id.insert(icc_attr);
+    std::set<unsigned> bath_id;
+    bath_id.insert(bath_attr);
+
+    // Construct ICC mesh network from mesh file
+    mesh.ConstructFromMeshReader(mesh_reader);
+    
+    nElements = mesh.GetNumLocalElements();
+
+    // Define boundary nodes as bath
+    double eleIdentify = 0;
+    for (DistributedTetrahedralMesh<PROBLEM_ELEMENT_DIM,PROBLEM_SPACE_DIM>::ElementIterator iter = mesh.GetElementIteratorBegin(); iter != mesh.GetElementIteratorEnd(); ++iter)
+    {
+      eleIdentify = iter->GetAttribute();
+      if (eleIdentify == icc_attr) 
+      {
+        for(int j = 0; j<=3; ++j)
+        {
+            iccNodes.insert(iter->GetNodeGlobalIndex(j));
+        }
+      }
+    }
+
+    // Print mesh summary
+    TRACE("Number of elements: " << nElements);
+    TRACE("Number of ICC nodes: " << iccNodes.size());
+    TRACE("Total number of nodes: " << mesh.GetNumAllNodes());
+
+    // Loads neural info and set up ParamConfig singleton instance
+    ParamConfig::InitInstance("projects/NeuralData/2DLarge.txt");
+    ParamConfig::GetInstance()->CreateGriddedControlRegions(0.0, 2.0, 20, 0.0, 3.0, 30);
+    ParamConfig::GetInstance()->MapNodeToControl(mesh);
+
+
+    // Set pacemaker location
+    ChastePoint<PROBLEM_SPACE_DIM> vertex1(0.0, 0.0);
+    ChastePoint<PROBLEM_SPACE_DIM> vertex2 (2.0, 0.8);
+
+    // Initialise problem with cells
+    ICCFactory<PROBLEM_SPACE_DIM> network_cells(iccNodes, &vertex1, &vertex2);
+    BidomainProblemNeural<PROBLEM_SPACE_DIM> bidomain_problem(&network_cells, true);
+    bidomain_problem.SetMesh( &mesh );
+
+    // Modify simulation config
+    HeartConfig::Instance()->Reset();
+    HeartConfig::Instance()->SetSimulationDuration(duration);
+    HeartConfig::Instance()->SetOutputDirectory(output_dir.c_str());
+    HeartConfig::Instance()->SetOutputFilenamePrefix("results");
+    HeartConfig::Instance()->SetTissueAndBathIdentifiers(ICC_id, bath_id);
+    HeartConfig::Instance()->SetIntracellularConductivities(Create_c_vector(0.12, 0.12));
+    HeartConfig::Instance()->SetExtracellularConductivities(Create_c_vector(0.2, 0.2));
+    HeartConfig::Instance()->SetSurfaceAreaToVolumeRatio(2000);
+    HeartConfig::Instance()->SetCapacitance(2.5);
+    HeartConfig::Instance()->SetVisualizeWithMeshalyzer(true);
+    HeartConfig::Instance()->SetOdePdeAndPrintingTimeSteps(0.1, 0.1, print_step);
+
+    // Update problem from config
+    bidomain_problem.SetWriteInfo();
+    bidomain_problem.Initialise();    // resets initial conditions and time to 0.0 ms
+
+    TRACE("Starting Solve");
+    // Solve problem
+    bidomain_problem.Solve();
+
+    // Print summary to terminal
+    HeartEventHandler::Headings();
+    HeartEventHandler::Report();
+  };
+};
+
+#endif 
