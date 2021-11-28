6a8221ea2bcf475812208bfc88b21dcb271f8ef2
Omkar Athavale <oa1234@live.com>
2021-11-28 20:08:15 +0000

    bug fix

--- src/ParamConfig.cpp
+++ src/ParamConfig.cpp
@@ -167,7 +167,7 @@ std::string TidyNeuralData::GetParamName(){
 double TidyNeuralData::GetParamVal(){
     if (GetParamName() == "excitatory_neural") {
         return CalibrationFunctions::Beta_Zhang2011(paramVals[currIndex]);
-    } else if (GetParamName() == "excitatory_neural") {
+    } else if (GetParamName() == "inhibitory_neural") {
         return CalibrationFunctions::GBKmax_Kim2003(paramVals[currIndex]);
     } else {
         return paramVals[currIndex];
