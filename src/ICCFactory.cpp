925cf4d5f7fcc459a13ae837f38006e560e2af41
Omkar Athavale <oa1234@live.com>
2021-12-01 21:27:27 +0000

    1.075 cor

--- src/ICCFactory.cpp
+++ src/ICCFactory.cpp
@@ -14,7 +14,7 @@ AbstractCardiacCell* ICCFactory<DIM>::CreateCardiacCellForTissueNode(Node<DIM>* 
     
     if (pacemaker.DoesContain(pNode->GetPoint()))
     {
-      cell->SetParameter("correction", 1.1);
+      cell->SetParameter("correction", 1.075);
     }
 
     return cell;
