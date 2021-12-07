b0dd10c5cf9bb73a572358ea19b9d3228a7fe1d0
Omkar Athavale <oa1234@live.com>
2021-12-07 05:50:41 +0000

    change to Stomach3D conductivity params

--- test/TestMinimal3D_Checkpoint.hpp
+++ test/TestMinimal3D_Checkpoint.hpp
@@ -109,10 +109,10 @@ class TestMinimal3D : public CxxTest::TestSuite
     HeartConfig::Instance()->SetOutputFilenamePrefix("results");
     HeartConfig::Instance()->SetTissueAndBathIdentifiers(ICC_id, bath_id);
     HeartConfig::Instance()->SetUseAbsoluteTolerance(2e-3);
-    HeartConfig::Instance()->SetIntracellularConductivities(Create_c_vector(0.024, 0.024, 0.024));
-    HeartConfig::Instance()->SetExtracellularConductivities(Create_c_vector(0.036, 0.036, 0.036));
+    HeartConfig::Instance()->SetIntracellularConductivities(Create_c_vector(0.00005, 0.05, 0.75));
+    HeartConfig::Instance()->SetExtracellularConductivities(Create_c_vector(0.00005, 0.05, 0.75));
     HeartConfig::Instance()->SetUseAbsoluteTolerance(2e-3); //Changed to get around the DIVERGED_ITS error default:2e-4
-    HeartConfig::Instance()->SetBathConductivity(0.02); // Bath capacitance
+    // HeartConfig::Instance()->SetBathConductivity(0.02); // Bath capacitance
     HeartConfig::Instance()->SetSurfaceAreaToVolumeRatio(2000);
     HeartConfig::Instance()->SetCapacitance(2.5);
     HeartConfig::Instance()->SetVisualizeWithMeshalyzer(true);        
