69e22b727f0b5d228bd0dd0eff44052b4955a946
Omkar Athavale <oa1234@live.com>
2022-02-14 02:20:26 +0000

    remaned output files

--- test/TestMinimal3D_Checkpoint.hpp
+++ test/TestMinimal3D_Checkpoint.hpp
@@ -37,7 +37,7 @@ class TestMinimal3D : public CxxTest::TestSuite
 
     // -------------- OPTIONS ----------------- //
     std::string mesh_ident = "stom_bath.1";
-    std::string output_dir = mesh_ident + "-thickenedICC";
+    std::string output_dir = mesh_ident + "-thickenedICC_monodomain";
     unsigned bath_attr = 1;
     unsigned icc_attr = 2;
     double duration = 18000.0;      // ms
--- test/TestMinimal3D_Restart.hpp
+++ test/TestMinimal3D_Restart.hpp
@@ -37,7 +37,7 @@ class TestMinimal3DRestart : public CxxTest::TestSuite
 
     // -------------- OPTIONS ----------------- //
     std::string mesh_ident = "stom_bath.1";
-    std::string chkpt_dir = mesh_ident + "-thickenedICC";
+    std::string chkpt_dir = mesh_ident + "-thickenedICC_monodomain";
     double added_duration = 30000.0;      // ms
     double print_step = 100.0;        // ms
     std::string output_dir = chkpt_dir + "_add30s";
