0b526e0118bf0664af709ebd7212b9a84ab9f5f9
Omkar Athavale <oa1234@live.com>
2022-02-15 04:26:00 +0000

    gradient for cor

--- src/ICCFactory.cpp
+++ src/ICCFactory.cpp
@@ -12,6 +12,26 @@ AbstractCardiacCell* ICCFactory<DIM>::CreateCardiacCellForTissueNode(Node<DIM>* 
   {
     CellDu2013_neuralFromCellML* cell = new CellDu2013_neuralFromCellML(this->mpSolver, this->mpZeroStimulus);
     
+    double xLoc = pnode->GetPoint()[0];
+    double zLoc = pnode->GetPoint()[2];
+
+    double xRef = 0.0;
+    double xExt = 0.9;
+
+    double zRef = -3.1;
+    double zExt = -4.7;
+
+    double pkVal = 1.0;
+    double minVal = 0.9;
+
+    double gradVal = (1.075 - (1.075-0.85) * fabs(xLoc-xRef)/(xExt-xRef) *  fabs(zLoc-zRef)/(zExt-zRef);
+
+    if (zLoc < -4.7) {
+      gradVal = 0.93;
+    }
+    
+    cell->SetParameter("correction", gradVal);
+
     if (pacemaker.DoesContain(pNode->GetPoint()))
     {
       cell->SetParameter("correction", 1.075);
