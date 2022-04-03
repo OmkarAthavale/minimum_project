2d9c1b89ff9743918260eeb9e85bf4d7c5d0f4d0
Omkar Athavale <oa1234@live.com>
2022-04-03 21:05:22 +0000

    file loc 2

--- src/ICCFactoryFull.cpp
+++ src/ICCFactoryFull.cpp
@@ -3,7 +3,7 @@
 template<unsigned DIM>
 void ICCFactoryFull<DIM>::ReadLaplaceFile()
 {
-	std::ifstream inLaplaceInfo("projects/mesh/scaffold_full/rat_scaffold_16_16_2.1_laplace_longi.txt");
+	std::ifstream inLaplaceInfo("src/projects/mesh/scaffold_full/rat_scaffold_16_16_2.1_laplace_longi.txt");
 	if(!inLaplaceInfo)
 	{
 	  EXCEPTION("Reading laplace solution error");
